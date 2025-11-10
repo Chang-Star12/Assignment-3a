11e4a353a4bb92935b605d0c9bc15db9e6100659
Chang Hwan <jijh5779@gmail.com>
2025-11-11 02:03:19 +0900

    Assignment-3a시작. 컨트롤인터페이스 공부

---
+++ Assignment-3a/Assignment-3a.h
@@ -0,0 +1,32 @@
+﻿
+// Assignment-3a.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CAssignment3aApp:
+// 이 클래스의 구현에 대해서는 Assignment-3a.cpp을(를) 참조하세요.
+//
+
+class CAssignment3aApp : public CWinApp
+{
+public:
+	CAssignment3aApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CAssignment3aApp theApp;
